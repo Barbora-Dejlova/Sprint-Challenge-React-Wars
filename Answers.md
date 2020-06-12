f55d0960058a4409529499217e15525243f87478
Barbora Dejlova <barboradejlova@BarborasMacBook.fios-router.home>
2020-06-12 13:26:21 -0400

    done but no stretch

--- Answers.md
+++ Answers.md
@@ -2,8 +2,17 @@
 
 1. What is React JS and what problems does it solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+React Js is a library which is declarative, component-based. React has some dependencies such as useState and useEffect that will allow you to do big things with less code. 
+
+
 1. Describe component state.
+
+Component state is an object that determines how that component renders and behaves. It will allow you to create components that are dynamic and interactive.
 
 1. Describe props.
 
+Properties are special keywords that are used fro passing data from one component to another.
+
 1. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+
+Side effects are things you want to do outside of the scope of the current function. You will use useEffect in order to sync effects in React.
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,56 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+// Write your Character component here
+
+const Cards = props => {
+    let {data} = props;
+
+    return (
+ 
+       <div>
+
+            { data.map(character => {
+                return (
+
+                    <StyledDiv>
+                        <StyledCard>
+
+                        <h1>{character.name}</h1>
+                        <h2>{character.gender}</h2>
+                        <h3>Mug Shot</h3>
+                        <StyledImg src={character.image}></StyledImg>
+
+                        </StyledCard>
+                    </StyledDiv>
+
+                )
+
+            })}
+            
+       
+       </div>
+
+    )
+
+}
+
+const StyledDiv = styled.div`
+flex-direction:column;
+ display: flex;
+ align-items:center;
+`;
+
+const StyledCard = styled.div`
+background-color: white;
+width: 30%;
+margin:1rem;
+padding:2%;
+border: dotted 2px red;
+border-radius:10%;
+`;
+const StyledImg = styled.img`
+border: red solid 2px;
+border-radius:20%;
+`;
+export default Cards
